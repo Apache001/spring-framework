db8875dd3e26284a52ede26250c6a901550b0b01
wangpengzhe <wangpengzhe@jd.com>
2019-11-21 16:04:23 +0800

    springDemo

---
+++ springDemo/src/main/java/com/wpz/Person.java
@@ -0,0 +1,16 @@
+package com.wpz;
+
+import lombok.Data;
+
+/**
+ * @author wangpengzhe
+ * @description
+ * @date 2019/11/21 15:55
+ * @copyright Copyright 2017-2027 JD.COM All Right Reserved
+ */
+@Data
+public class Person {
+
+    private String name;
+    private Integer age;
+}
