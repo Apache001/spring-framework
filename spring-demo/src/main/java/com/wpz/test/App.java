6a619882f34f3a1123f23245afebb5df99699f97
wangpengzhe <wangpengzhe@bytedance.com>
2020-12-24 11:11:48 +0800

    test

--- spring-demo/src/main/java/com/wpz/test/App.java
+++ spring-demo/src/main/java/com/wpz/test/App.java
@@ -1,6 +1,7 @@
 package com.wpz.test;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,6 +10,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  *
  * @author bytedance
  */
+@Slf4j
 public class App {
 	public static void main( String[] args ) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
